4a92bfa1b3762d8a65325f02c6df3a7068616902
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-03-28 15:10:32 -0400

    roulette

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # roulette
+
+![Roulette](public/thumbnail.png)
+
+## Languages Used:
+### HTML, CSS, JS, Node, EJS
+
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. run `node server.js`
+2. Navigate to `localhost:8080`
---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+module.exports = {
+
+    'url' : 'mongodb+srv://hayden:hayden@russianroulette-rtqeg.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
